be6e666626c98782a6259999b74eda3f36d6b56f
Bryan Nelson <nelsonbm@coolsite.biz>
2019-06-05 14:23:25 -0700

    started styling buttons

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import './Buttons.css';
 
 class Buttons extends React.Component {
 
     render() {
         return(
             <div>
-                <Button />
-                <Button />
-                <Button />
-                <Button />
-                <Button />
-                <Button />
+                <Button id="seven" buttonText="7"/>
+                <Button id="eight" buttonText= "8"/>
+                <Button id="nine" buttonText="9"/>
+                <Button id="divide" buttonText="/"/>
+                <Button id="four" buttonText="4"/>
+                <Button id="five" buttonText="5"/>
+                <Button id="six" buttonText="6"/>
+                <Button id="multiply" buttonText="*"/>
+                <Button id="one" buttonText="1"/>
+                <Button id="two" buttonText="2"/>
+                <Button id="three" buttonText="3"/>
+                <Button id="subtract" buttonText="-"/>
+                <Button id="zero" buttonText="0"/>
+                <Button id="decimal" buttonText="."/>
+                <Button id="equals" buttonText="="/>
+                <Button id="add" buttonText="+"/>
+                <Button id="clear" buttonText="CE"/>
             </div>
         );
     }
@@ -19,8 +31,11 @@ class Buttons extends React.Component {
 }
 
 function Button(props) {
+    console.log(props);
+
     return(
-        <div>button</div>
+        <div className={!isNaN(props.buttonText) ? 'digit' : 'operator'}>{props.buttonText}</div>
+        
     );
 }
 
